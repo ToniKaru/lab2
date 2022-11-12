70461f2c81d7b5ad31cf6109dac9629ac948dddf
ToniKaru <toni.karu@gmail.com>
2022-11-12 20:08:50 +0100

    Add plan your week view

--- src/components/HeaderData.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>
-    <h1>Toni's Task Planning Tool!</h1>
-  </div>
-</template>
-
-<style scoped></style>
---
+++ src/components/PlanYourWeek.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ExerciseItem from "@/components/ExerciseItem.vue";
+</script>
+
+<template>
+  <div>
+    <h2>Monday</h2>
+    <ExerciseItem />
+    <h2>Tuesday</h2>
+    <ExerciseItem />
+    <h2>Wednesday</h2>
+    <ExerciseItem />
+    <h2>Thursday</h2>
+    <ExerciseItem />
+    <h2>Friday</h2>
+    <ExerciseItem />
+    <h2>Saturday</h2>
+    <ExerciseItem />
+    <h2>Sunday</h2>
+    <ExerciseItem />
+  </div>
+</template>
+
+<style scoped></style>
