3a6d1a4ccfba34c225cdb5abcdcfd9dcf59f3602
ToniKaru <toni.karu@gmail.com>
2022-11-14 00:11:02 +0100

    Remove unused components

--- src/components/HomeDetails.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>
-    <h3></h3>
-  </div>
-</template>
-
-<style scoped></style>
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import HomeDetails from '../components/HomeDetails.vue';
-</script>
-
-<template>
-  <main>
-    <HomeDetails />
-  </main>
-</template>
