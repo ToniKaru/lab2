4b2de2dc17920da6f2529a7a91095523ab29f96f
ToniKaru <toni.karu@gmail.com>
2022-11-13 23:59:52 +0100

    Update routing

--- src/components/HeaderDetail.vue
+++ src/components/HeaderDetail.vue
@@ -4,9 +4,6 @@
   <div>
     <h1>Your Exercise Planning Tool</h1>
     <p>Create a custom exercise schedule!</p>
-    <nav>
-      <RouterLink to="/planner">Planner</RouterLink>
-    </nav>
   </div>
 </template>
 
@@ -14,15 +11,5 @@
 h1,
 p {
   margin: 5px;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 1rem;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import PlannerView from "../views/PlanningView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/planner",
-      name: "planner",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/PlanningView.vue"),
-    },
-
-    {
-      path: "/schedule",
-      name: "schedule",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/ScheduleView.vue"),
+      name: "",
+      component: PlannerView,
     },
   ],
 });
